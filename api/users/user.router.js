be03066c5f15e703461fa954718c2d444484f02d
Mohammad Saifur Rahman <saifur.rahman@sslwireless.com>
2021-11-07 14:46:35 +0600

    updated

--- api/users/user.router.js
+++
@@ -1,6 +0,0 @@
-const {createUser} = require('./user.controller.js');
-const router = require("express").Router()
-
-router.post("/createUser", createUser)
-
-module.exports = router
--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const app = express()
 
 // routes
+console.log("=================1=============");
 const userRouter = require('./api/users/user.router');
+console.log("=================2=============");
 
-app.use("/api/v/users", userRouter)
+app.use(express.json())
+
+app.use("/api/v1/users", userRouter)
 
 app.listen(process.env.APP_PORT || 3000, ()=>{
     console.log("Server up & running");
